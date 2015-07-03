e59f70a8aa35740d97f3a84868e650bfbf5bdae1
dajacobs <dajacobs.tab@gmail.com>
2015-07-02 22:53:11 -0500

    minor edits

--- search/combo1.sh
+++ search/combo1.sh
@@ -1,8 +1,9 @@
 # File system being searched.
 path='/'
 
+# Conditional check for sudo privileges.
 if  [ "$(whoami)" != "root" ]; then
-	echo "Please please run again, with sudo command."
+	echo "Please run again with sudo command."
 else
 	echo "Total number of files that begin with lowercase 'a' to 'z'."
 	find $path -type f -name '[a-z]*' -printf "%f\n" 2</dev/null | cut -c1 | sort |uniq -c
