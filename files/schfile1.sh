e1d32282a12792aa4c1488620704dabf00583889
dajacobs <dajacobs.tab@gmail.com>
2015-07-05 00:31:42 -0500

    edits

--- files/schfile1.sh
+++ files/schfile1.sh
@@ -4,6 +4,7 @@ path='/'
 # Conditional check for sudo privileges.
 if  [ "$(whoami)" != "root" ]; then
 	echo "Please run again with sudo command."
+# Find, cut, and sort files on system using using unique occurrences. Output errors to trash.	
 else
 	echo "Total number of files that begin with lowercase 'a' to 'z'."
 	find $path -type f -name '[a-z]*' -printf "%f\n" 2</dev/null | cut -c1 | sort |uniq -c
--- files/schfile2.sh
+++ files/schfile2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# Create a temp variable to store outputs.
 TMPFILE=/tmp/A_TEMPFILE
+# Fill temp variable with initial empty string.
 echo "" > ${A_TEMPFILE}
-
+# Find and cut files starting from root and output to temp variable.
 find / -type f -exec basename {} \; | cut -d\/ -f2 | cut -c1 > ${TMPFILE}
-	
+# Loop using a bucket list.	
 for bucket in a b c d e f g h i j k l m n o p q r s t u v w x y z
+# Get the lines of the file from the bucket list and temp variable.
 do
 	echo -n "bucket ${bucket}:"
 	grep ${bucket} ${TMPFILE} | wc -l
