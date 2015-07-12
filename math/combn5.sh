1f17ec8fd5c80d3302ed79dccde278943d82dc39
dajacobs <dajacobs.tab@gmail.com>
2015-07-12 08:55:50 -0500

    combinations

---
+++ math/combn5.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "The probability of guessing the password for a 5-digit (with each being 0-9) is 1 in $((10*5))."
+echo "The probability of guessing the password for a 3-digit (with each being 0-99) is 1 in $((100*3))."
